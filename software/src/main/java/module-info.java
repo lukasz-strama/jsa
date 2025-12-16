06e481fcd0702ffa9350ad029e2b526945d74164
Łukasz Strama <lukasz-strama@outlook.com>
2025-12-16 13:44:38 +0100

    Java: Configure Maven dependencies and Java Modules

--- software/src/main/java/module-info.java
+++ software/src/main/java/module-info.java
@@ -3,8 +3,12 @@ module pl.polsl.rtsa {
     requires javafx.fxml;
     requires com.fazecast.jSerialComm;
     requires org.apache.commons.lang3;
-    requires JTransforms; 
+    requires JTransforms;
 
     exports pl.polsl.rtsa;
+    exports pl.polsl.rtsa.model;
+    exports pl.polsl.rtsa.hardware;
+
+    opens pl.polsl.rtsa to javafx.fxml;
     opens pl.polsl.rtsa.controller to javafx.fxml;
 }
